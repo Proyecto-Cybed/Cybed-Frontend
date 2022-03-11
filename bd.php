2ee9bffc5c3a2b3f7b15e6564e6b118be2ca7740
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-03-11 13:52:04 +0100

    fixed env vars

--- bd.php
+++ bd.php
@@ -1,8 +1,10 @@
 <?php
-$bd_env_var = getenv('bd_env_var');
+$bdDsn = getenv('BD_DSN');
+$bdUser = getenv('BD_USER');
+$bdPassword = getenv('BD_PASSWORD');
 
 try {
-		$bd = new PDO($bd_env_var);
+		$bd = new PDO($bdDsn, $bdUser, $bdPassword);
 	} catch (PDOException $e) {
 		exit($e->getMessage());
 	}

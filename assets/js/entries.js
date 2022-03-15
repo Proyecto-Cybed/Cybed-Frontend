43a832398a01317d15b8af1bc5787907423d1655
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-03-15 09:29:52 +0100

    Last msg and author at forum

--- assets/js/entries.js
+++ assets/js/entries.js
@@ -1,16 +1,45 @@
+function stringDatetoDate(date) {
+
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    let splitedDate = date.split("-")
+
+    let YYYY = splitedDate[0];
+    let M = splitedDate[1];
+    let DD = splitedDate[2];
+    let hh = splitedDate[3];
+    let mm = splitedDate[4];
+    let ss = splitedDate[5];
+
+    let veraguaDate = new Date(YYYY, M, DD, hh, mm, ss);
+
+    return veraguaDate.toLocaleDateString('es-ES', options)
+
+}
+
 $.ajax({
     type: "GET",
     dataType: "json",
-    url: "http://cybed-api.herokuapp.com/api/entradas/",
+    url: "https://cybed-api.herokuapp.com/api/entradas/",
     crossDomain: true,
 
-    success: function(json) {
+    success: function (json) {
         const results = json["results"]
         for (let i = 0; i < json["count"]; i++) {
             const entry = results[i];
 
-            console.log(entry);
-            var id = entry["id"]
+            let id = entry["id"];
+            let totalMessages = entry["mensajes"]["count"];
+            let lastMessage = "No hay mensajes";
+
+            if (totalMessages != 0) {
+                userLastMessage = entry["mensajes"]["results"].slice(-1)[0].usuario;
+                stringDateLastMessage = entry["mensajes"]["results"].slice(-1)[0].fecha;
+
+                let dateLastMessage = stringDatetoDate(stringDateLastMessage);
+
+                lastMessage = "Por " + userLastMessage + " el " + dateLastMessage;
+            }
 
 
             $("#entries").append(
@@ -21,19 +50,19 @@ $.ajax({
                 "</a></td>" +
                 '</td>' +
                 '<td>' +
-                entry["mensajes"]["count"] +
+                totalMessages +
                 "</td>" +
                 '<td>' +
                 entry["usuario"] +
                 "</td>" +
                 '<td>' +
-                "Author y fecha del mensaje " +
+                lastMessage +
                 "</td>"
             );
             document.getElementById('enlace' + entry["id"]).setAttribute('href', 'entry.php?' + 'id=' + id);
         }
     },
-    error: function(xhr, status) {
+    error: function (xhr, status) {
 
         console.log("error");
 
